f1620ffdb01a255a6c667ccd0ed61d201abece38
Chris Pickens <Cpickens1270@gmail.com>
2023-01-22 07:10:15 -0700

    3rd commit

--- FirstAssignment/Program.vb
+++ FirstAssignment/Program.vb
@@ -5,5 +5,6 @@ Module Program
         Console.WriteLine("Hello World!")
         Console.WriteLine("This line is the first change to be commited to Github.")
         Console.WriteLine("This line is the second change to be commited to Github.")
+        Console.WriteLine("This line is the third change to be commited to Github.")
     End Sub
 End Module
