8728fb09db9ed97b3727ed6e1069d4eb6c7c033c
Chris Pickens <Cpickens1270@gmail.com>
2023-01-22 07:09:56 -0700

    2nd commit

--- FirstAssignment/Program.vb
+++ FirstAssignment/Program.vb
@@ -4,5 +4,6 @@ Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
         Console.WriteLine("This line is the first change to be commited to Github.")
+        Console.WriteLine("This line is the second change to be commited to Github.")
     End Sub
 End Module
