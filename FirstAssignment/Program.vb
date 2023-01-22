b511748424d936a96761ca0cadd2ea1ece8b9ea2
Chris Pickens <Cpickens1270@gmail.com>
2023-01-22 07:09:28 -0700

    first commit

--- FirstAssignment/Program.vb
+++ FirstAssignment/Program.vb
@@ -3,5 +3,6 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+        Console.WriteLine("This line is the first change to be commited to Github.")
     End Sub
 End Module
